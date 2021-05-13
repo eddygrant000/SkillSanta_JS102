3a8cab6b1e76d7f0845a385b4575b3e492723594
Sachin Saini <sachin.saini@ap2v.com>
2021-05-13 20:54:59 +0530

    js operator

---
+++ operator.js
@@ -0,0 +1,80 @@
+// document.write("Hi all <br>");
+
+// What are operator 
+//   + : operator
+// 2, 5 : operand
+// console.log(2 + 5);
+// Airthematic operator : Maths operator
+
+// addition : + 
+// subs : -
+// mul  : *
+// division : /
+// remainder: %
+// exponent : **
+
+// Assignement : =
+// add and assign : +=
+// sub and assign : -=
+// %=
+// *=
+// **=
+// /=
+// let a = 8;
+// a **= 3;
+// console.log(a);
+
+// String Operator
+//  +,
+//  +=
+
+// let first_name = "Sachin";
+// let last_name = "Saini";
+
+// first_name += " " + last_name;
+// // let fullname = first_name + " " + last_name;
+
+// console.log(first_name);
+
+// Increment and Decrement
+// ++
+// --
+
+// let a = 10;
+
+// console.log(a--);
+// console.log(a);
+
+// console.log(++a);
+
+// Comparision Operators
+
+//    == : equal
+//    === : identical
+
+//    != : not equal
+//    !== : not identical
+//    < : less 
+//    > : greater than
+//    <= : less than equal
+//    >= : greate than equal
+// -------------------------------
+
+//     true or false
+
+// Logical Operator
+//  && : and --> 
+//  || : or
+//  !  : not
+
+// now ---> va
+// true  && false = false
+// true  && true  = true
+// false && true  = false
+// false && false = false
+
+// true  || false = true
+// true  || true  = true
+// false || true  = true
+// false || false = false
+// https://quizizz.com/join?gc=278158
