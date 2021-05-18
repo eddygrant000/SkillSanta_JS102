df0bea8ec85147983fdf9a6dc9ec9c38e0d89ed4
Sachin Saini <sachin.saini@ap2v.com>
2021-05-18 20:56:32 +0530

    while loop

---
+++ loop.js
@@ -0,0 +1,75 @@
+// Loop 
+// ATM --> three // 
+
+// participants --> 97
+
+/// this is me
+
+// For -> basic , in , of 
+// while --> do while
+
+// while (condition){
+//     statement1;
+//     statement2;
+//     statement3;
+// }
+
+
+// 10 < 15 : true
+// 11 < 15 : true
+// 12 < 15 : true
+// 13 < 15 : true
+// 14 < 15 : true
+// 15 < 15 : true
+// 16 < 15 : false
+// 20 <= 15: false
+
+// let num = 10;
+// //    10 > 20:false
+// while (num > 20) {
+//     document.write(num + "<br>");
+//     num--
+// }
+
+// do{
+//     loop body1
+//     loop body2
+// }while(condition)
+
+// let num = 10;
+
+// do {
+//     document.write(num + "<br>");
+//     num++
+// } while (num < 15);
+
+//      10 < 5 : false
+// authentication ==>
+// intagram --> username , password
+
+// three ->
+// let username = "eddygrant000";
+// let password = "red123";
+
+// let attempt = 1;
+
+// while (attempt <= 3) {
+//     let xy = "Enter Username " + attempt + " attempt: ";
+//     let user_username = prompt(xy);
+//     let user_password = prompt("Enter password: ");
+//     if (user_username == username) {
+//         if (user_password == password) {
+//             document.body.innerHTML = '';
+//             document.write("Login Successfull!");
+//             break;
+//         } else {
+//             document.body.innerHTML = '';
+//             document.write("Password is wrong!");
+//         }
+//     } else {
+//         document.body.innerHTML = '';
+//         document.write("Username is Not Defined.");
+//     }
+//     attempt++
+// }
+// https://quizizz.com/join?gc=382118
