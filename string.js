2e16ce031b880e543651094a0b3bf91f6feaa616
Sachin Saini <sachin.saini@ap2v.com>
2021-05-21 20:59:19 +0530

    string manipulation

---
+++ string.js
@@ -0,0 +1,65 @@
+// String 
+// let result = typeof(num1);
+
+// document.write(result);
+// 
+
+//  ' '   : single quotes
+//  " "   : double quotes
+//  ` `   : backticks ~` , sabstitution
+
+// let num1 = "200";
+
+// let name1 = "Shashank";
+
+// let name2 = 'name1';
+
+// document.write(name1);
+
+// let statement1 = "manish's father name is Manoj ";
+
+// document.write(statement1);
+
+// function greeting(msg) {
+//     document.write(msg);
+// }
+
+// Hey Bhushan! Good Evening!
+
+
+
+// greeting(`Hey ${prompt("Enter your name")} Good Evening!`);
+
+
+// let username = prompt("Enter your name: ");
+
+// let msg = `Hey ${username} Good Evening!`;
+
+// document.write(msg)
+
+// let username = "Eddy Grant";
+
+// document.write(username.length);
+
+// username = username.toLowerCase(); // this is  a inplace function
+
+// document.write(username.toLowerCase());
+
+// let email2 = 'eddygrant000@gmail.com';
+// //            0123456789.........
+// // 
+// let email1 = 'sachin@example.com';
+
+//                ...-3-2-1
+// let username = "Eddy Grant";
+//indexing =>   0123456789.....
+
+// document.write(email1.slice(-4));
+
+// slicing
+// document.write(username[0]);
+// document.write(username.charAt(10))
+// console.log(username[11]);
+// document.write(email1.slice(0, 6));
+
+// console.log(email2.split("@"))
